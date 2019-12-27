367ff8b877a4617fcc76138308dcca3048674ffc
FortesThiago <48772859+FortesThiago@users.noreply.github.com>
2019-12-27 12:51:22 -0300

    adcionando cadastro e relatorio de pessoa

--- src/cc/code/Main.java
+++ src/cc/code/Main.java
@@ -1,6 +1,6 @@
 package cc.code;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Main {
@@ -8,13 +8,41 @@ public class Main {
         Vector v = new Vector();
         ReportDePessoas r = new ReportDePessoas();
         Pessoa p = new Pessoa();
-        p.name = "Fulano";
-        p.cpf = "0033435457";
-        ArrayList<String> pessoaTelefonesFixos = new ArrayList<String>();
-        pessoaTelefonesFixos.add("8765343");
-        p.telefonesCelulares = pessoaTelefonesFixos;
-        v.add(p);
+        Scanner ler = new Scanner(System.in);
 
-        r.print_e_vai(v);
+        int opcao;
+        do {
+            System.out.println("1- Cadastrar");
+            System.out.println("2- Exibir Dados");
+            System.out.println("0- Sair");
+
+            opcao = ler.nextInt();
+            switch (opcao) {
+                case 1:
+                    p.cadastrarPessoa();
+                    v.add(p);
+                    break;
+                case 2:
+                    if (v.isEmpty()) {
+                        System.out.println("Lista vazia");
+                    } else {
+                        r.print_e_vai(v);
+                    }
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("ERROR");
+            }
+        } while (opcao != 0);
     }
+
+//        p.name = "Fulano";
+//        p.cpf = "0033435457";
+//        ArrayList<String> pessoaTelefonesFixos = new ArrayList<String>();
+//        pessoaTelefonesFixos.add("8765343");
+//        p.telefonesCelulares = pessoaTelefonesFixos;
+//        v.add(p);
+//
+//        r.print_e_vai(v);
 }
--- src/cc/code/Pessoa.java
+++ src/cc/code/Pessoa.java
@@ -1,12 +1,80 @@
 package cc.code;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Pessoa {
 
     public String name;
     public String cpf;
-    public Integer idade;
+    public Integer age;
     public List<String> telefonesFixos;
     public List<String> telefonesCelulares;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String name, String cpf, Integer age, List<String> telefonesFixos, List<String> telefonesCelulares) {
+        this.name = name;
+        this.cpf = cpf;
+        this.age = age;
+        this.telefonesFixos = telefonesFixos;
+        this.telefonesCelulares = telefonesCelulares;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public List<String> getTelefonesFixos() {
+        return telefonesFixos;
+    }
+
+    public void setTelefonesFixos(List<String> telefonesFixos) {
+        this.telefonesFixos = telefonesFixos;
+    }
+
+    public List<String> getTelefonesCelulares() {
+        return telefonesCelulares;
+    }
+
+    public void setTelefonesCelulares(List<String> telefonesCelulares) {
+        this.telefonesCelulares = telefonesCelulares;
+    }
+
+    public void cadastrarPessoa() {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite o seu nome: ");
+        this.setName(ler.nextLine());
+        System.out.println("Digite o cpf");
+        this.setCpf(ler.nextLine());
+        System.out.println("Digite a sua idade: ");
+        this.setAge(ler.nextInt());
+        System.out.println("Digite seu numero de telefone fixo");
+        this.setTelefonesCelulares(Collections.singletonList(ler.nextLine()));
+        System.out.println("Digite o seu numero de celular");
+        this.setTelefonesCelulares(Collections.singletonList(ler.nextLine()));
+
+    }
 }
